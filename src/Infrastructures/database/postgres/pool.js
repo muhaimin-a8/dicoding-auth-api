aee3cf9f540d16d2176a1207d2c8ba4a4499fb72
Muhaimin Abdillah <90732533+muhaimin-a8@users.noreply.github.com>
2022-12-07 03:19:23 +0000

    add Custom Error and fixing eslint

--- src/Infrastructures/database/postgres/pool.js
+++ src/Infrastructures/database/postgres/pool.js
@@ -1,15 +1,16 @@
+/* eslint-disable no-undef */
 /* istanbul ignore file */
 
 const { Pool } = require('pg');
 
 const testConfig = {
-    host: proccess.env.PGHOST_TEST,
-    port: proccess.env.PGPORT_TEST,
-    user: proccess.env.PGUSER_TEST,
-    password: proccess.env.PGPASSWORD_TEST,
-    database: proccess.env.PGDATABASE_TEST,
+  host: proccess.env.PGHOST_TEST,
+  port: proccess.env.PGPORT_TEST,
+  user: proccess.env.PGUSER_TEST,
+  password: proccess.env.PGPASSWORD_TEST,
+  database: proccess.env.PGDATABASE_TEST,
 };
 
 const pool = process.env.NODE_ENV === 'test' ? new Pool(testConfig) : new Pool();
 
-module.exports = pool
+module.exports = pool;
