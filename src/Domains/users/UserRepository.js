080d7be064e7ceda04a5923984189c85086a211d
Muhaimin Abdillah <muhaiminabdillah85@gmail.com>
2022-12-09 08:36:55 +0700

    fixing lint

--- src/Domains/users/UserRepository.js
+++ src/Domains/users/UserRepository.js
@@ -1,11 +1,12 @@
+/* eslint-disable no-unused-vars */
 class UserRepository {
-    async addUser(registerUser) {
-        throw new Error('USER_REPOSITORY.METHOD_NOT_IMPLEMENTED');
-    }
+  async addUser(registerUser) {
+    throw new Error('USER_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
 
-    async verifyAvailableUsername(username) {
-        throw new Error('USER_REPOSITORY.METHOD_NOT_IMPLEMENTED');
-    }
+  async verifyAvailableUsername(username) {
+    throw new Error('USER_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
 }
 
 module.exports = UserRepository;
--- src/Domains/users/_test/UserRepository.test.js
+++ src/Domains/users/_test/UserRepository.test.js
@@ -1,12 +1,12 @@
 const UserRepository = require('../UserRepository');
 
 describe('UserRepository interface', () => {
-    it('should throw error when invoke abstract behavior', async () => {
-        // Arrange
-        const userRepository = new UserRepository();
+  it('should throw error when invoke abstract behavior', async () => {
+    // Arrange
+    const userRepository = new UserRepository();
 
-        // Action and Assert
-        await expect(userRepository.addUser({})).rejects.toThrowError('USER_REPOSITORY.METHOD_NOT_IMPLEMENTED');
-        await expect(userRepository.verifyAvailableUsername('')).rejects.toThrowError('USER_REPOSITORY.METHOD_NOT_IMPLEMENTED');
-    });
+    // Action and Assert
+    await expect(userRepository.addUser({})).rejects.toThrowError('USER_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+    await expect(userRepository.verifyAvailableUsername('')).rejects.toThrowError('USER_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  });
 });
