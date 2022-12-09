56268be0032cf0d7e793b115b5342dc2b79cec52
Muhaimin Abdillah <muhaiminabdillah85@gmail.com>
2022-12-09 09:52:56 +0700

    change proccess to process

--- src/Infrastructures/database/postgres/pool.js
+++ src/Infrastructures/database/postgres/pool.js
@@ -4,11 +4,11 @@
 const { Pool } = require('pg');
 
 const testConfig = {
-  host: proccess.env.PGHOST_TEST,
-  port: proccess.env.PGPORT_TEST,
-  user: proccess.env.PGUSER_TEST,
-  password: proccess.env.PGPASSWORD_TEST,
-  database: proccess.env.PGDATABASE_TEST,
+  host: process.env.PGHOST_TEST,
+  port: process.env.PGPORT_TEST,
+  user: process.env.PGUSER_TEST,
+  password: process.env.PGPASSWORD_TEST,
+  database: process.env.PGDATABASE_TEST,
 };
 
 const pool = process.env.NODE_ENV === 'test' ? new Pool(testConfig) : new Pool();
